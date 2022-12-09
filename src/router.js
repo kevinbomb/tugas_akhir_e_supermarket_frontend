415b5bf266b24836d634a2608d3ff4730ce1341e
pricellvalencia <pricellvalencia2222@gmail.com>
2022-12-09 10:42:21 +0700

    Frontend

---
+++ src/router.js
@@ -0,0 +1,41 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+function importComponent(path){
+    return () => import(`./components/${path}.vue`);
+}
+
+const router = new VueRouter({
+    mode: "history",
+    routes: [
+        //Login Page
+        {
+            path: "/login",
+            name: "Login",
+            component: importComponent("LoginPage"),
+        },
+        //Register Page
+        {
+            path: "/register",
+            name: "Register",
+            component: importComponent("RegisterPage"),
+        },
+        //Dashboard Page
+        {
+            path: "/",
+            name: "admin",
+            component: importComponent("DashboardPage"),
+            children: [
+                //Dashboard
+                {
+                    path: "/",
+                    name: "Root",
+                    component: importComponent("DashboardIndex"),
+                },
+        },
+    ],
+});
+
+export default router;
