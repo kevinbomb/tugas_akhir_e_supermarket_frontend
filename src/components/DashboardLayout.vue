010baf32620d9bbf739df214658aa0343a95c445
melatisukma1666 <melatisukma083@gmail.com>
2022-12-15 18:22:42 +0700

    dashboardLayout

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -1,5 +1,52 @@
 <template>
-    <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium quos beatae tempore magni vero blanditiis perferendis, aliquid, necessitatibus fugit eum id aperiam molestias tempora delectus dicta voluptatibus doloremque et officia.
+    <div class="dasboard">
+        <v-navigation-drawer class="fullheight" width="256" v-model="drawer"
+        absolute temporary color="grey lighten-5" >
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="title">SUPERMARKET</v-list-item-title>
+                    <v-list-item-subtitle>by GROUP E</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list dense nav>
+                <v-list-item v-for="item in items" :key="item.title" link 
+                color="light-blue darken-4" tag="router-link" :to="item.to">
+                    <v-list-item-content>
+                        <v-list-item-title>{{item.title}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar color="red lighten-3" dark>
+            <v-app-bar-nav-icon @click="drawer = true" 
+            color="white"></v-app-bar-nav-icon>
+            <VSpacer />
+        </v-app-bar>
+        <div class="fullheight pa-5">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
+<script>
+export default {
+    name: "DashboardIndex",
+    data() {
+        return{
+            drawer: false,
+            group: null,
+            items: [
+                {title: "Dashboard", to: "/"},
+                {title: "Supplier", to: "/supplier"},
+            ],
+        };
+    },
+};
+</script>
+<style scoped>
+    .fullheight {
+        min-height: 100vh !important;
+    }
+</style>
